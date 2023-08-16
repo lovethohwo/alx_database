21c7212fed833143c93486ca28229e393f63e2de
judyloveth <judyohwo29@gmail.com>
2023-08-16 13:11:00 +0100

    edit

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+CREATE TABLE IF NOT EXISTS
+    hbtn_0c_0.first_table
+    (id INT,
+    name VARCHAR(256)
+    );
+
+/*
+*** BELOW IS THE RESULT ***
+
+mysql> CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+Query OK, 1 row affected (0.14 sec)
+
+mysql> CREATE TABLE IF NOT EXISTS
+    -> hbtn_0c_0.first_table
+    -> (id INT,
+    -> name VARCHAR(256)
+    -> );
+Query OK, 0 rows affected (1.64 sec)
+
+mysql>
+*/
