5bd6d6561871856651cda6cc3fd827450c213e1a
judyloveth <judyohwo29@gmail.com>
2023-08-16 13:42:16 +0100

    count rows by id

---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,18 @@
+SELECT COUNT(*)
+FROM hbtn_0c_0.first_table
+WHERE id = 89;
+
+/*
+*** BELOW IS THE RESULT ***
+
+mysql> SELECT COUNT(*)
+    -> FROM hbtn_0c_0.first_table
+    -> WHERE id = 89;
++----------+
+| COUNT(*) |
++----------+
+|        1 |
++----------+
+1 row in set (0.06 sec)
+
+*/
