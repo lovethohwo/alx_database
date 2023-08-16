75bacff7e90b3d414e3c5192378306f58a37dc28
judyloveth <judyohwo29@gmail.com>
2023-08-16 13:34:06 +0100

    first values inserted

---
+++ SQL_introduction/7-insert_values.sql
@@ -0,0 +1,11 @@
+INSERT INTO hbtn_0c_0.first_table (id, name)
+VALUES (89, 'Holberton School');
+
+/*
+*** BELOW IS THE RESULT ***
+
+mysql> INSERT INTO hbtn_0c_0.first_table (id, name)
+    -> VALUES (89, 'Holberton School');
+Query OK, 1 row affected (0.28 sec)
+
+*/
