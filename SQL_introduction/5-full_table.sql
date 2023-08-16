ebe3b658f4d5e0c84ca0120790d8078a3a6a7514
judyloveth <judyohwo29@gmail.com>
2023-08-16 19:22:29 +0100

    full table desc update

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,31 +1,31 @@
 -- FULL DESCRIPTION OF A TABLE
-
-
-first_table CREATE TABLE IF NOT EXISTS
-   hbtn_test_db_5.first_table
-    (id INT(11) NOT NULL AUTO_INCREMENT,
-    name VARCHAR(128) DEFAULT NULL,
-    ENGINE=InnoDB DEFAULT CHARSET=latin1
+--Create database first
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_5;
+CREATE TABLE IF NOT EXISTS
+hbtn_test_db_5.first_table
+    (id INT(11),
+    name VARCHAR(128)
 );
+
+--Describe full details
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'hbtn_test_db_5'
+AND TABLE_NAME = 'first_table';
+
 
 /*
 *** BELOW IS THE RESULT ***
-CREATETABLE`first_table`(
-`id`intNOTNULLAUTO_INCREMENT,
-`name`varchar(128)DEFAULTNULL,
-`c`char(1)DEFAULTNULL,
-`created_at`dateDEFAULTNULL,
-PRIMARYKEY(`id`)
-)ENGINE=InnoDBDEFAULTCHARSET=utf8mb4COLLATE=utf8mb4_0900_ai_ci
-mysql> USE hbtn_0c_0;
-Database changed
-mysql> DESC first_table;
-+-------+--------------+------+-----+---------+-------+
-| Field | Type         | Null | Key | Default | Extra |
-+-------+--------------+------+-----+---------+-------+
-| id    | int          | YES  |     | NULL    |       |
-| name  | varchar(256) | YES  |     | NULL    |       |
-+-------+--------------+------+-----+---------+-------+
-2 rows in set (0.00 sec)
 
+ SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY, EXTRA
+    -> FROM INFORMATION_SCHEMA.COLUMNS
+    -> WHERE TABLE_SCHEMA = 'hbtn_test_db_5'
+    -> AND TABLE_NAME = 'first_table';
++-------------+--------------+-------------+----------------+------------+-------+
+| COLUMN_NAME | COLUMN_TYPE  | IS_NULLABLE | COLUMN_DEFAULT | COLUMN_KEY | EXTRA |
++-------------+--------------+-------------+----------------+------------+-------+
+| id          | int          | YES         | NULL           |            |       |
+| name        | varchar(128) | YES         | NULL           |            |       |
++-------------+--------------+-------------+----------------+------------+-------+
+2 rows in set (0.04 sec)
 */
