8a862e90c513b9d86f8d22d9746adc15905dd7f8
judyloveth <judyohwo29@gmail.com>
2023-08-16 18:37:51 +0100

    Describe a table UPDATE

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,11 +1,25 @@
 -- Describe a table
-USE hbtn_0c_0;
+
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_5;
+CREATE TABLE IF NOT EXISTS
+   hbtn_test_db_5.first_table
+    (id INT,
+    name VARCHAR(256)
+    );
+
+USE hbtn_test_db_5;
 
 DESC first_table;
 
 /*
 *** BELOW IS THE RESULT ***
-
+CREATETABLE`first_table`(
+`id`intNOTNULLAUTO_INCREMENT,
+`name`varchar(128)DEFAULTNULL,
+`c`char(1)DEFAULTNULL,
+`created_at`dateDEFAULTNULL,
+PRIMARYKEY(`id`)
+)ENGINE=InnoDBDEFAULTCHARSET=utf8mb4COLLATE=utf8mb4_0900_ai_ci
 mysql> USE hbtn_0c_0;
 Database changed
 mysql> DESC first_table;
