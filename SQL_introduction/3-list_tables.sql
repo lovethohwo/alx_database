e6fc2dbb5ece74ac147bd7a896516b799e8211c6
judyloveth <judyohwo29@gmail.com>
2023-08-16 18:20:12 +0100

    UPDATE LIST TABLE

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,51 +1,17 @@
 -- Display list of tables
-USE mysql;
+USE hbtn_test_db_0;
 SHOW TABLES;
 /*
-mysql> USE mysql;
+mysql> use hbtn_test_db_0;
 Database changed
 mysql> SHOW TABLES;
-+------------------------------------------------------+
-| Tables_in_mysql                                      |
-+------------------------------------------------------+
-| columns_priv                                         |
-| component                                            |
-| db                                                   |
-| default_roles                                        |
-| engine_cost                                          |
-| func                                                 |
-| general_log                                          |
-| global_grants                                        |
-| gtid_executed                                        |
-| help_category                                        |
-| help_keyword                                         |
-| help_relation                                        |
-| help_topic                                           |
-| innodb_index_stats                                   |
-| innodb_table_stats                                   |
-| ndb_binlog_index                                     |
-| password_history                                     |
-| plugin                                               |
-| procs_priv                                           |
-| proxies_priv                                         |
-| replication_asynchronous_connection_failover         |
-| replication_asynchronous_connection_failover_managed |
-| replication_group_configuration_version              |
-| replication_group_member_actions                     |
-| role_edges                                           |
-| server_cost                                          |
-| servers                                              |
-| slave_master_info                                    |
-| slave_relay_log_info                                 |
-| slave_worker_info                                    |
-| slow_log                                             |
-| tables_priv                                          |
-| time_zone                                            |
-| time_zone_leap_second                                |
-| time_zone_name                                       |
-| time_zone_transition                                 |
-| time_zone_transition_type                            |
-| user                                                 |
-+------------------------------------------------------+
-38 rows in set (0.09 sec)
++--------------------------+
+| Tables_in_hbtn_test_db_0 |
++--------------------------+
+| holbteron_0              |
+| holbteron_1              |
+| holbteron_2              |
+| holbteron_3              |
++--------------------------+
+4 rows in set (0.00 sec)
 */
