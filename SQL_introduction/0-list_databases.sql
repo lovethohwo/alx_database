e5f536bc8df1c2c8f50a357bd497ae244f7b795f
judyloveth <judyohwo29@gmail.com>
2023-08-16 11:41:28 +0100

    list databases

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,1 +1,12 @@
-
+mysql> show databases;
++--------------------+
+| Database           |
++--------------------+
+| information_schema |
+| mysql              |
+| performance_schema |
+| sakila             |
+| sys                |
+| world              |
++--------------------+
+6 rows in set (0.05 sec)
