8130874fb4d59e32356dd7dd09d5cde39602225d
judyloveth <judyohwo29@gmail.com>
2023-08-16 13:09:18 +0100

    Create firsfull table descriptiont_table

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,19 @@
+USE hbtn_0c_0;
+
+DESC first_table;
+
+/*
+*** BELOW IS THE RESULT ***
+
+mysql> USE hbtn_0c_0;
+Database changed
+mysql> DESC first_table;
++-------+--------------+------+-----+---------+-------+
+| Field | Type         | Null | Key | Default | Extra |
++-------+--------------+------+-----+---------+-------+
+| id    | int          | YES  |     | NULL    |       |
+| name  | varchar(256) | YES  |     | NULL    |       |
++-------+--------------+------+-----+---------+-------+
+2 rows in set (0.00 sec)
+
+*/
