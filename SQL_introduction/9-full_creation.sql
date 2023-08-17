81ff98f2a6b5854210e2aaeccf71b9dcde739e16
judyloveth <judyohwo29@gmail.com>
2023-08-17 09:58:44 +0100

    full creation updated

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -4,7 +4,7 @@ hbtn_0c_0.second_table (id INT,
 name VARCHAR(256),
 score INT);
 -- Then insert the values
-INSERT INTO hbtn_0c_0.second_table (id, name, score)
+INSERT INTO second_table (id, name, score)
 VALUES (1, 'John', 10),
 (2, 'Alex', 3),
 (3, 'Bob', 14),
