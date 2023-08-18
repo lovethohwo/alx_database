78c81bae93c6ea40d01e6bf8d51f9575159b18a6
judyloveth <judyohwo29@gmail.com>
2023-08-18 12:49:13 +0100

    update of full table

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,21 +1,2 @@
--- FULL DESCRIPTION OF A TABLE
---Create database first
-
+-- creating a full table
 SHOW CREATE TABLE first_tables;
-
-
-/*
-*** BELOW IS THE RESULT ***
-
- SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY, EXTRA
-    -> FROM INFORMATION_SCHEMA.COLUMNS
-    -> WHERE TABLE_SCHEMA = 'hbtn_test_db_5'
-    -> AND TABLE_NAME = 'first_table';
-+-------------+--------------+-------------+----------------+------------+-------+
-| COLUMN_NAME | COLUMN_TYPE  | IS_NULLABLE | COLUMN_DEFAULT | COLUMN_KEY | EXTRA |
-+-------------+--------------+-------------+----------------+------------+-------+
-| id          | int          | YES         | NULL           |            |       |
-| name        | varchar(128) | YES         | NULL           |            |       |
-+-------------+--------------+-------------+----------------+------------+-------+
-2 rows in set (0.04 sec)
-*/
