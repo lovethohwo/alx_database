a09d002e443794f054d7a55f0dedcc7990c79408
judyloveth <judyohwo29@gmail.com>
2023-08-18 13:10:33 +0100

    updated first table

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,7 +1,5 @@
 -- Create a new database
-CREATE DATABASE IF NOT EXISTS hbtn_test_db_4;
-CREATE TABLE IF NOT EXISTS
-   hbtn_test_db_4.first_table
+CREATE TABLE IF NOT EXISTS first_table
     (id INT,
     name VARCHAR(256)
     );
