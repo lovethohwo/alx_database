e51799e5f187a34a71b98024f98b758213f90054
judyloveth <judyohwo29@gmail.com>
2023-08-31 09:08:55 +0100

    task 6

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- Creating a database named hbtn_0d_usa
+CREATE SCHEMA IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+-- creating a table named states
+CREATE TABLE IF NOT EXISTS states(
+id INT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(256) NOT NULL);
